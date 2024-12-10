fa947b592a75997f71932602152b602dbb4ac4fa
Skoropey Viacheslav <vskorop@gmail.com>
2024-12-10 19:27:49 +0700

    fix: build path, added tests scripts

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { Transport } from '@nestjs/microservices';
-import { join } from 'path';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.connectMicroservice({
-    transport: Transport.GRPC,
-    options: {
-      package: 'auth',
-      protoPath: join(__dirname, '../../src/auth/auth.proto'),
-    },
-  });
 
-  await app.listen(3000);
+
+  await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
+
+
+
